a77ad94d190bdd525af7d75c615f97b8a9bf4afc
Sajed Jalil <sjalil@gmu.edu>
2023-11-27 11:30:07 -0500

    Updated available models

--- src/webview/scripts/main.js
+++ src/webview/scripts/main.js
@@ -150,9 +150,7 @@ function restoreDroppedItems(droppedItems) {
 // Assuming apiMappings is available in main.js
 const apiMappings = {
   'GPT-4': "gpt-4",
-  'GPT-4 32k': "gpt-4-32k",
   'GPT-3.5': "gpt-3.5-turbo",
-  'GPT-3.5 16k': "gpt-3.5-turbo-16k",
   'Bard': "text-bison-001",
   'AI21':"j2-ultra",
   'Cohere': 'command'
